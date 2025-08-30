98611aa244d55268ba0dcb0402617255c5151cbc
MinhKy <minhkynhieu@gmail.com>
2025-08-30 22:33:03 +0700

    Prj1 Commit.

---
+++ FirstPrj/src/test/test.java
@@ -0,0 +1,14 @@
+/**
+ * @description: Test 
+ * @author: NhieuMinhKy_24718221
+ * @version: 1.0
+ * @created: Aug 22, 2025 4:51:27 PM
+ */
+package test;
+public class test {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
